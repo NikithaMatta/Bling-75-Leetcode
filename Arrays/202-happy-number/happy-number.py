844fc7296c76fb7e918744680a442665fa856565
Nikitha Matta <100593166+NikithaMatta@users.noreply.github.com>
2025-05-08 18:34:30 +0530

    Time: 8 ms (16.46%) | Memory: 12.4 MB (52.46%) - LeetSync

--- Arrays/202-happy-number/happy-number.py
+++ Arrays/202-happy-number/happy-number.py
@@ -5,11 +5,9 @@ class Solution(object):
         :rtype: bool
         """
         seen = set()
-        
         while n != 1:
             if n in seen:
                 return False
             seen.add(n)
             n = sum(int(digit)**2 for digit in str(n))
-        
         return True
